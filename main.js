ff27e8850915a6e01a81a655547dc1a280245d6a
Melissa <snowflake12@centurylink.net>
2018-11-14 12:33:07 -0800

    Added text to index and main

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+const BASE_URL = "https://zagster-service.herouapp.com"
 $(updateView)
 
 function updateView() {
-    alert("using jQuery!")
+    $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+}
+
+function updateRideCount (data)  {
+    numberOfRides = data.updateRideCount
+    $( "h2#rideCount"). html (numberOfRides)
 }
